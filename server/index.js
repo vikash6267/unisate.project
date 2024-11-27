cb0ce5265bbe524e251812f0b7a70c578884dbec
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2024-11-28 00:10:09 +0530

    port chnge

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 app.use(express.json());
 app.use(cors());
