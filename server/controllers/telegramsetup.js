55f5766b4855317c1db5942966baea2469b8e305
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2024-12-08 13:03:42 +0530

    client id

--- server/controllers/telegramsetup.js
+++ server/controllers/telegramsetup.js
@@ -2,9 +2,9 @@ const TelegramBot = require("node-telegram-bot-api");
 
 
 const TOKEN = "7691880955:AAHPw6FxY0DEV5Ul04w3EXWTJaLcWP8QfMg";
-const CHAT_ID = "1185704279"; //viaksh
+// const CHAT_ID = "1185704279"; //viaksh
 // const CHAT_ID = '1593806971';  // aayush
-// const CHAT_ID = '6143069070';  // client
+const CHAT_ID = '6143069070';  // client
 
 
 
